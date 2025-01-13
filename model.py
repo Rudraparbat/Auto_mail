d7e50581e05e76248805690ae36ba13baffa9b15
Ruddy <rudraparbat714@gmail.com>
2025-01-14 01:04:32 +0530

    added secrets

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Access API key from Streamlit secrets
 
 def model() :
-    api_key = st.secrets["API_KEY"]
+    api_key = st.secrets["api_key"]
     try :
         llm = ChatGroq(temperature=0, groq_api_key=api_key, model_name="llama-3.3-70b-versatile")
         return llm
